0dc76c04ec6a9045c0c8226904370769f998cf02
Tony Zito <tonyzito@gmail.com>
2014-12-30 12:34:21 -0800

    cleaned up devise alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_class_name(name)
+      case name
+      when 'notice' then 'success'
+      when 'alert'  then 'danger'
+      else name
+      end
+  end
 end
