f5f3e246e3efdadcc33ce7154cbb46ce266f9e08
Tony Zito <tonyzito@gmail.com>
2014-12-30 11:29:56 -0800

    configured mandrill/smtp

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,13 @@ MOVES ={
   "seer" => "reveal",
   "angel" => "protect"
 }
+
+ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
