6cdda1f75c6fd1398152b6ee0d025d4c3942f7a1
Tony Zito <tonyzito@gmail.com>
2014-12-31 13:49:19 -0800

    first pass at email on game start

---
+++ app/mailers/games_mailer.rb
@@ -0,0 +1,12 @@
+class GamesMailer < ActionMailer::Base
+  default from: "wolfmaster@wolfy.herokuapp.com"
+  
+  def game_started(game)
+    @game = game
+    for player in game.players
+      user = player.user
+      # send
+      mail(to: user.email, subject: "Werewolf game \"#{@game.name}\" has begun!")
+    end
+  end
+end
---
+++ test/mailers/previews/games_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/games_mailer
+class GamesMailerPreview < ActionMailer::Preview
+
+end
